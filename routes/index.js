620f0f3e1a44aa6f850c84b8b0016337371797d6
Jumpei Asanuma <jun64pei89@gmail.com>
2021-06-20 13:00:45 +0900

    タスクを追加して、リロードしても同じ内容がレンダリングされる

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+let todos = [];
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Todo App' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Todo App',
+    todos: todos,
+  });
+});
+
+router.post('/', function (req, res, next) {
+  const todo = req.body.add;
+  todos.push(todo);
+  res.redirect('/')
 });
 
 module.exports = router;
