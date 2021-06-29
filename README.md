c5cde70237b452f3ec5c657529dc61e7053620a7
Jumpei Asanuma <jun64pei89@gmail.com>
2021-06-29 21:27:35 +0900

    todoをデータベースから取得して、表示させる

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
 # Nodeを使ってTODOアプリを作成
-[参考記事](https://zenn.dev/wkb/books/node-tutorial/viewer/todo_04)
+[参考記事](https://zenn.dev/wkb/books/node-tutorial/viewer/todo_05)
 
-#### Chapter 12
+#### Chapter 13
 
 
+確認URL
+
+http://localhost:3000
+
+
+
+---
+#### 補足情報
+
+mysqlのパスワード変更方法
+https://uxmilk.jp/12396
+
+```
+mysqladmin password 新しいパスワード -u ユーザー名 -p
+```
+新しいパスワードの部分を新しく登録したいパスワードに置き換えて入力
+ユーザー名も該当のユーザー名に設定
+
+```
+mysqladmin password MyNewPass! -u root -p
+Enter password:
+```
+こんな感じで入力すると、
+Enter passwordって出てくるから、
+以前のパスワードを入力して設定完了
+ちなみに非ログイン状態で行う
+
+ユーザー情報の確認方法
+https://www.dbonline.jp/mysql/user/index11.html
+
+ログイン中に
+```
+select user(), current_user();
+```
+上記で確認できる、他の方法は、URL参照
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,45 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
 
-let todos = [];
-/* GET home page. */
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Pei89jun64!',
+  database: 'todo_app'
+})
+
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Todo App',
-    todos: todos,
-  });
+  connection.query(
+    `select * from tasks;`,
+    (error, results) => {
+      console.log(error);
+      console.log(results);
+      res.render('index', {
+        title: 'Todo App',
+        todos: results,
+      });
+    }
+  );
 });
 
 router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if(err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
   const todo = req.body.add;
-  todos.push(todo);
-  res.redirect('/')
+  connection.query(
+    `insert into tasks (user_id, content) value (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
